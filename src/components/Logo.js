20b04c97f215bdc1082215fae56b39db63779c2e
KevinPozuelos <2723810200106@ingenieria.usac.edu.gt>
2021-10-21 14:22:39 -0600

    Carro de compras terminado

---
+++ src/components/Logo.js
@@ -0,0 +1,14 @@
+import { Component } from "react"
+
+
+class Logo extends Component {
+    render(){
+        
+        return(
+            <div>
+                <img  src="/Logo.png" alt='Logo' style={{width:'300px', height:'100px'}}/>
+            </div>
+        )
+    }
+}
+export default Logo
